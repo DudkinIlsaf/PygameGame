61f51f6286f7a29831b710dd44404f0dce9afec3
Lenovo <ilsaf.dudkin.2015@yandex.ru>
2020-01-04 14:12:13 +0300

    Function for head and body positions

--- Project.py
+++ Project.py
@@ -18,3 +18,25 @@ class Snake():
                 self.change_to == "UP" and not self.direction == "DOWN",
                 self.change_to == "DOWN" and not self.direction == "UP")):
             self.direction = self.change_to
+
+    def change_head_position(self):
+        if self.direction == "RIGHT":
+            self.snake_head_pos[0] += 10
+        elif self.direction == "LEFT":
+            self.snake_head_pos[0] -= 10
+        elif self.direction == "UP":
+            self.snake_head_pos[1] -= 10
+        elif self.direction == "DOWN":
+            self.snake_head_pos[1] += 10
+
+    def snake_body_mechanism(
+            self, score, food_pos, screen_width, screen_height):
+        self.snake_body.insert(0, list(self.snake_head_pos))
+        if (self.snake_head_pos[0] == food_pos[0] and
+                self.snake_head_pos[1] == food_pos[1]):
+            food_pos = [random.randrange(1, screen_width/10)*10,
+                        random.randrange(1, screen_height/10)*10]
+            score += 1
+        else:
+            self.snake_body.pop()
+        return score, food_pos
