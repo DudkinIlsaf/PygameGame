11abd931c5259a3ec21c994d2e5f96ada9ba9e9f
Lenovo <ilsaf.dudkin.2015@yandex.ru>
2020-01-04 14:17:09 +0300

    Function for drawing food

--- Project.py
+++ Project.py
@@ -11,3 +11,9 @@ class Food():
         self.food_size_y = 10
         self.food_pos = [random.randrange(1, screen_width/10)*10,
                          random.randrange(1, screen_height/10)*10]
+
+    def draw_food(self, play_surface):
+        pygame.draw.rect(
+            play_surface, self.food_color, pygame.Rect(
+                self.food_pos[0], self.food_pos[1],
+                self.food_size_x, self.food_size_y))
